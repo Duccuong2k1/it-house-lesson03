8987479c86693aa943f9d479f047549c0d5ae88d
duc cuong <doduccuong2001@gmail.com>
2022-04-09 15:39:58 +0700

    apollo graqhql

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import run from './exam';
-run();
+// import run from './exam';
+// run();
+
+import app, { startExpressServer } from "./express";
+import graphqlServer from './apollo';
+
+startExpressServer();
+
+
+graphqlServer.start().then(() => {
+    graphqlServer.applyMiddleware({app});
+});
